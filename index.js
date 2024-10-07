ead7ac4f3a7303d13b0c0d9f58b267ed7a131395
Rasheed <119102828+Rasheed-ibn-Rafi@users.noreply.github.com>
2024-10-07 10:57:22 +0530

    feat: partially implement cool func

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function func1(params) {
   console.log("this code is func 1");
 }
+function coolFeat(params) {
+  console.log("this is a cool feat branch func");
+}
 function func2(params) {
   console.log("this code is func 2");
 }
